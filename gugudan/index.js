d19deabb66d678faf2f638db7d2375853f40d46a
Lee Sang Mi <lsm32333@naver.com>
2020-05-18 17:24:26 +0900

    Update index.js

--- gugudan/index.js
+++ gugudan/index.js
@@ -1,16 +1,16 @@
-const r = document.querySelector("#result");
+const r = document.querySelector('#result');
 
-document.querySelector("#click").addEventListener("click", () => {
-  const a = document.querySelector("#first").value;
-  const b = document.querySelector("#second").value;
+document.querySelector('#click').addEventListener('click', () => {
+  const a = document.querySelector('#first').value;
+  const b = document.querySelector('#second').value;
   if (a) {
     if (b) {
       const c = a * b;
       r.textContent = c;
     } else {
-      r.textContent = "두 번째 값 입력하세요.";
+      r.textContent = '두 번째 값을 입력하세요.';
     }
   } else {
-    r.textContent = "첫 번째 값 입력하세요.";
+    r.textContent = '첫 번째 값을 입력하세요.';
   }
 });
